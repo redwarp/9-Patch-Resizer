8cb74a60c02ae5ba617a2fa984865c8e9bae1414
Benoît Vermont <benoit.vermont@fr.airtag.com>
2015-02-12 17:35:14 +0100

    Adds the ability to keep original image file for same density, for jpeg (quite ugly, will need a refactoring)

--- src/net/redwarp/tool/resizer/worker/ImageWriter.java
+++ src/net/redwarp/tool/resizer/worker/ImageWriter.java
@@ -16,45 +16,71 @@
 
 package net.redwarp.tool.resizer.worker;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Iterator;
+
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.stream.FileImageOutputStream;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
 
 /**
- * User: benoit.vermont@airtag.com
- * Date: 28/10/14
- * Time: 19:24
+ * User: benoit.vermont@airtag.com Date: 28/10/14 Time: 19:24
  */
 public class ImageWriter {
-    public static void write(BufferedImage outputImage, Output output, File outputFile) throws IOException {
-        if (output == Output.JPG) {
-            // Need to strip alpha;
-            BufferedImage img = new BufferedImage(outputImage.getWidth(), outputImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-            Graphics2D g2d = img.createGraphics();
-            g2d.drawImage(outputImage, 0, 0, null);
-            g2d.dispose();
-            outputImage = img;
 
-            Iterator<javax.imageio.ImageWriter> itor = ImageIO.getImageWritersByFormatName("jpeg");
-            javax.imageio.ImageWriter writer = itor.next();
-            ImageWriteParam imageWriteParam = writer.getDefaultWriteParam();
-            imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            imageWriteParam.setCompressionQuality(0.9f);
+  public static void write(BufferedImage outputImage, Output output, File outputFile)
+      throws IOException {
+    if (output == Output.JPG) {
+      // Need to strip alpha;
+      BufferedImage
+          img =
+          new BufferedImage(outputImage.getWidth(), outputImage.getHeight(),
+                            BufferedImage.TYPE_INT_RGB);
+      Graphics2D g2d = img.createGraphics();
+      g2d.drawImage(outputImage, 0, 0, null);
+      g2d.dispose();
+      outputImage = img;
 
-            FileImageOutputStream outputStream = new FileImageOutputStream(outputFile);
-            writer.setOutput(outputStream);
-            IIOImage image = new IIOImage(outputImage, null, null);
-            writer.write(null, image, imageWriteParam);
-            writer.dispose();
+      Iterator<javax.imageio.ImageWriter> itor = ImageIO.getImageWritersByFormatName("jpeg");
+      javax.imageio.ImageWriter writer = itor.next();
+      ImageWriteParam imageWriteParam = writer.getDefaultWriteParam();
+      imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      imageWriteParam.setCompressionQuality(0.8f);
 
-        } else {
-            ImageIO.write(outputImage, output.getFormat(), outputFile);
-        }
+      FileImageOutputStream outputStream = new FileImageOutputStream(outputFile);
+      writer.setOutput(outputStream);
+      IIOImage image = new IIOImage(outputImage, null, null);
+      writer.write(null, image, imageWriteParam);
+      writer.dispose();
+
+    } else {
+      ImageIO.write(outputImage, output.getFormat(), outputFile);
     }
+  }
+
+  public static void copy(File inputFile, File outputFile) throws IOException {
+    if (inputFile != null && outputFile != null) {
+      FileChannel sourceChannel = null;
+      FileChannel destChannel = null;
+      try {
+        sourceChannel = new FileInputStream(inputFile).getChannel();
+        destChannel = new FileOutputStream(outputFile).getChannel();
+        destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+      } finally {
+        if (sourceChannel != null) {
+          sourceChannel.close();
+        }
+        if (destChannel != null) {
+          destChannel.close();
+        }
+      }
+    }
+  }
 }
--- src/net/redwarp/tool/resizer/worker/ScreenDensity.java
+++ src/net/redwarp/tool/resizer/worker/ScreenDensity.java
@@ -15,161 +15,192 @@
  */
 package net.redwarp.tool.resizer.worker;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
-import javax.swing.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.*;
+
 public class ScreenDensity {
-    private static final String KEY_DENSITIES = "densities";
-    private static final String KEY_SOURCE = "source";
-    public static final String DENSITIES_PATHNAME = "./densities.json";
-    private float scale;
-    private String name;
 
-    private boolean active;
+  private static final String KEY_DENSITIES = "densities";
+  private static final String KEY_SOURCE = "source";
+  private static final String KEY_KEEP_SAME_DENSITY_FILE = "keep_same_density_file";
+  public static final String DENSITIES_PATHNAME = "./densities.json";
+  private float scale;
+  private String name;
 
-    private static List<ScreenDensity> list = null;
-    private static ScreenDensity defaultInputDensity = null;
+  private boolean active;
 
-    static {
-        load();
-    }
+  private static List<ScreenDensity> list = null;
+  private static ScreenDensity defaultInputDensity = null;
+  private static boolean keepSameDensityFile = false;
 
-    private static void load() {
-        try {
-            Gson gson = new Gson();
-            JsonParser parser = new JsonParser();
-            InputStream preferenceStream;
-            try {
-                preferenceStream = new FileInputStream(new File(DENSITIES_PATHNAME));
-            } catch (Exception e) {
-                preferenceStream = ScreenDensity.class.getClassLoader().getResourceAsStream("misc/densities.json");
-            }
-            JsonObject densitiesObject = parser.parse(new InputStreamReader(preferenceStream)).getAsJsonObject();
-            JsonArray densitiesArray = densitiesObject.get(KEY_DENSITIES).getAsJsonArray();
+  static {
+    load();
+  }
 
-            Type listType = new TypeToken<List<ScreenDensity>>() {
-            }.getType();
-            list = gson.fromJson(densitiesArray, listType);
-            String defaultDensityName = densitiesObject.get(KEY_SOURCE).getAsString();
-            for (ScreenDensity density : list) {
-                if (density.getName().equals(defaultDensityName)) {
-                    defaultInputDensity = density;
-                    break;
-                }
-            }
-            if (defaultInputDensity == null) {
-                defaultInputDensity = list.get(0);
-            }
-        } catch (Exception e) {
-            list = new ArrayList<ScreenDensity>();
-            list.add(new ScreenDensity("xhdpi", 2.0f, true));
-            defaultInputDensity = list.get(0);
+  private static void load() {
+    try {
+      Gson gson = new Gson();
+      JsonParser parser = new JsonParser();
+      InputStream preferenceStream;
+      try {
+        preferenceStream = new FileInputStream(new File(DENSITIES_PATHNAME));
+      } catch (Exception e) {
+        preferenceStream =
+            ScreenDensity.class.getClassLoader().getResourceAsStream("misc/densities.json");
+      }
+      JsonObject
+          densitiesObject =
+          parser.parse(new InputStreamReader(preferenceStream)).getAsJsonObject();
+      JsonArray densitiesArray = densitiesObject.get(KEY_DENSITIES).getAsJsonArray();
+
+      Type listType = new TypeToken<List<ScreenDensity>>() {
+      }.getType();
+      list = gson.fromJson(densitiesArray, listType);
+      String defaultDensityName = densitiesObject.get(KEY_SOURCE).getAsString();
+      for (ScreenDensity density : list) {
+        if (density.getName().equals(defaultDensityName)) {
+          defaultInputDensity = density;
+          break;
         }
+      }
+      if (defaultInputDensity == null) {
+        defaultInputDensity = list.get(0);
+      }
+
+      JsonElement keepSameDensityElement = densitiesObject.get(
+          KEY_KEEP_SAME_DENSITY_FILE);
+      if (keepSameDensityElement != null) {
+        keepSameDensityFile = keepSameDensityElement.getAsBoolean();
+      } else {
+        keepSameDensityFile = false;
+      }
+
+    } catch (Exception e) {
+      list = new ArrayList<ScreenDensity>();
+      list.add(new ScreenDensity("xhdpi", 2.0f, true));
+      defaultInputDensity = list.get(0);
+    }
+  }
+
+  public static void save(JButton saveButton) {
+    saveButton.setEnabled(false);
+    JsonObject rootObject = new JsonObject();
+    // Save source
+    rootObject.addProperty(KEY_SOURCE, defaultInputDensity.getName());
+
+    // Save densities
+    Gson gson = new Gson();
+    Type listOfDensityType = new TypeToken<List<ScreenDensity>>() {
+    }.getType();
+    JsonElement densities = gson.toJsonTree(list, listOfDensityType);
+    rootObject.add(KEY_DENSITIES, densities);
+
+    SaveWorker worker = new SaveWorker(saveButton, rootObject.toString());
+    worker.execute();
+  }
+
+  private ScreenDensity(String name, float density, boolean active) {
+    this.scale = density;
+    this.name = name;
+    this.active = active;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public float getScale() {
+    return this.scale;
+  }
+
+  public static List<ScreenDensity> getSupportedScreenDensity() {
+    return list;
+  }
+
+  public static ScreenDensity getDefaultInputDensity() {
+    return defaultInputDensity;
+  }
+
+  // A flag to indicate if we should copy the original image, if the input and output are of the same density.
+  public static boolean shouldKeepSameDensityFile() {
+    return keepSameDensityFile;
+  }
+
+  public static void setDefaultInputDensity(ScreenDensity density) {
+    defaultInputDensity = density;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof ScreenDensity) {
+      return this.name.equals(((ScreenDensity) obj).getName());
+    }
+    return false;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  public static class SaveWorker extends SwingWorker<Void, Void> {
+
+    private final String mSavePayload;
+    private final JButton mButton;
+
+    public SaveWorker(JButton saveButton, String savePayload) {
+      mButton = saveButton;
+      mSavePayload = savePayload;
     }
 
-    public static void save(JButton saveButton) {
-        saveButton.setEnabled(false);
-        JsonObject rootObject = new JsonObject();
-        // Save source
-        rootObject.addProperty(KEY_SOURCE, defaultInputDensity.getName());
 
-        // Save densities
-        Gson gson = new Gson();
-        Type listOfDensityType = new TypeToken<List<ScreenDensity>>() {
-        }.getType();
-        JsonElement densities = gson.toJsonTree(list, listOfDensityType);
-        rootObject.add(KEY_DENSITIES, densities);
+    @Override
+    protected Void doInBackground() throws Exception {
+      FileOutputStream fos = null;
+      try {
+        fos = new FileOutputStream(DENSITIES_PATHNAME);
+        PrintWriter writer = new PrintWriter(fos);
+        writer.write(mSavePayload);
 
-        SaveWorker worker = new SaveWorker(saveButton, rootObject.toString());
-        worker.execute();
-    }
+        writer.close();
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        System.out.println("Couldn't save");
+      }
 
-    private ScreenDensity(String name, float density, boolean active) {
-        this.scale = density;
-        this.name = name;
-        this.active = active;
+      return null;
     }
 
     @Override
-    public String toString() {
-        return this.name;
+    protected void done() {
+      if (mButton != null) {
+        mButton.setEnabled(true);
+      }
     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public float getScale() {
-        return this.scale;
-    }
-
-    public static List<ScreenDensity> getSupportedScreenDensity() {
-        return list;
-    }
-
-    public static ScreenDensity getDefaultInputDensity() {
-        return defaultInputDensity;
-    }
-
-    public static void setDefaultInputDensity(ScreenDensity density) {
-        defaultInputDensity = density;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof ScreenDensity) {
-            return this.name.equals(((ScreenDensity) obj).getName());
-        }
-        return false;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public static class SaveWorker extends SwingWorker<Void, Void> {
-        private final String mSavePayload;
-        private final JButton mButton;
-
-        public SaveWorker(JButton saveButton, String savePayload) {
-            mButton = saveButton;
-            mSavePayload = savePayload;
-        }
-
-
-
-        @Override
-        protected Void doInBackground() throws Exception {
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(DENSITIES_PATHNAME);
-                PrintWriter writer = new PrintWriter(fos);
-                writer.write(mSavePayload);
-
-                writer.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                System.out.println("Couldn't save");
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void done() {
-            if (mButton != null) {
-                mButton.setEnabled(true);
-            }
-        }
-    }
+  }
 }
