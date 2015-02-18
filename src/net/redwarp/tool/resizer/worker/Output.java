f5067b5ef94a4e7208f2c6812114c185cf0cd7e7
redwarp <redwarp@gmail.com>
2015-02-18 17:29:59 +0100

    Update license (I'm not so sure if I'm supposed to do it or...)

--- src/net/redwarp/tool/resizer/worker/Output.java
+++ src/net/redwarp/tool/resizer/worker/Output.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2013 Redwarp
+ * Copyright 2013-2015 Redwarp
  */
 
 package net.redwarp.tool.resizer.worker;
